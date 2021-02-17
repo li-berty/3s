bfe2eacc89c2fd9e4bca17d19b44300058537cb7
Berty Li <46299378+li-berty@users.noreply.github.com>
2021-02-17 21:38:09 +0300

    IP-ping

---
+++ iping.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+R="\e[1;31m" G="\e[1;32m" Y="\e[1;33m" N="\e[0m"
+
+for i in `cat IP.list`
+do  
+  if ping -c1 $i &> /dev/null
+  then
+    echo -e $G "$i is UP" $N
+  else
+    echo -e $R "$i is DOWN" $N
+  fi
+done
+echo -e $Y"DONE!" $N
