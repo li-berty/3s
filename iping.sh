2324d453203902cc8fe8d821110d2bf9d5462ef1
Berty Li <46299378+li-berty@users.noreply.github.com>
2021-12-23 00:19:25 +0300

    iping.sh

--- iping.sh
+++ iping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-R="\e[1;31m" G="\e[1;32m" Y="\e[1;33m" N="\e[0m"
+R='\e[1;31m' G='\e[1;32m' Y='\e[1;33m' N='\e[0m'
 echo -e $Y"Welcome, $(whoami | sed 's/^./\U&\E/')!" $N
 
 for i in `cat IP.list`
