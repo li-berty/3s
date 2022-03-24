f7c639634a452e2acbe54a2897bdc46b435341c5
li-berty <46299378+li-berty@users.noreply.github.com>
2022-03-25 02:32:33 +0300

    iping

--- iping.sh
+++ iping.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+clear
 R='\e[1;31m' G='\e[1;32m' Y='\e[1;33m' N='\e[0m'
-echo -e $Y"Welcome, $(whoami | sed 's/^./\U&\E/')!" $N
 
-for i in `cat IP.list`
+for target in `cat IP.list`
 do  
-  if ping -c1 $i &> /dev/null
-  then
-    echo -e $G "$i is UP" $N
-  else
-    echo -e $R "$i is DOWN" $N
-  fi
+	if ping -c1 $target &> /dev/null; then
+		echo -e $G "$target is UP" $N
+		ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
+		curl https://ipapi.co/$ip/csv/ >> ip.csv; echo >> ip.csv
+	else
+		echo -e $R "$i is DOWN" $N
+	fi
 done
+
+read -p "Input URL or IP address: " target; ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
+curl https://ipapi.co/$ip/csv/ >> ip.csv; echo >> ip.csv
+
 echo -e $Y"DONE!" $N
