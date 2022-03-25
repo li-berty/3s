ae9a5e5b80e66bc1be7b7e615c35019051c74671
li-berty <46299378+li-berty@users.noreply.github.com>
2022-03-25 14:55:21 +0300

    iping

--- iping.sh
+++ iping.sh
@@ -2,7 +2,7 @@
 
 clear
 R='\e[1;31m' G='\e[1;32m' Y='\e[1;33m' N='\e[0m'
-dir=`dirname $0`
+dir=`dirname $0`	#start script directory
 
 echo -e $R"
 WARNING :
@@ -30,18 +30,19 @@ elif [[ $key = 2 ]]; then
 	exe=json
 fi
 
-for target in `cat $dir/IP.list`
+for target in `cat $dir/IP.list`	#read IP list from file
 do  
-	if ping -c1 $target &> /dev/null; then
+	if ping -c1 $target &> /dev/null; then						#find out if the server is working
 		echo -e $G"$target is UP"$N
 		ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
-		curl https://ipapi.co/$ip/$exe/ >> $dir/ip.$exe; echo >> $dir/ip.$exe
+		curl https://ipapi.co/$ip/$exe/ >> $dir/ip.$exe; echo >> $dir/ip.$exe	#write information about target to file
 	else
 		echo -e $R"$target is DOWN"$N
 	fi
 done
 
-read -p "Input URL or IP address: " target; ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
-curl https://ipapi.co/$ip/$exe/ >> $dir/ip.$exe; echo >> $dir/ip.$exe
+read -p "Input URL or IP address: " target						#add URL or IP address to get information
+ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`				
+curl https://ipapi.co/$ip/$exe/ >> $dir/ip.$exe; echo >> $dir/ip.$exe			#adding information about target to file
 
 echo -e $G"DONE!" $Y"Inspect created $dir/ip.$exe file."$N
