89e18b40e28914f4c441a6eb4d1ccf1434d3c0ec
li-berty <46299378+li-berty@users.noreply.github.com>
2022-03-25 11:01:10 +0300

    iping

--- iping.sh
+++ iping.sh
@@ -3,18 +3,38 @@
 clear
 R='\e[1;31m' G='\e[1;32m' Y='\e[1;33m' N='\e[0m'
 
+echo -e $Y"Select Output File Format (json, by default):
+1 - json
+2 - csv
+3 - xml
+4 - yaml
+---------------------------------------------"
+read -s -n1 key
+
+if [[ $key = 1 ]]; then
+	exe=json
+elif [[ $key = 2 ]]; then
+	exe=csv
+ elif [[ $key = 3 ]]; then
+	exe=xml
+  elif [[ $key = 4 ]]; then
+	exe=yaml
+   else
+	exe=json
+fi
+
 for target in `cat IP.list`
 do  
 	if ping -c1 $target &> /dev/null; then
-		echo -e $G "$target is UP" $N
+		echo -e $G"$target is UP" $N
 		ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
-		curl https://ipapi.co/$ip/csv/ >> ip.csv; echo >> ip.csv
+		curl https://ipapi.co/$ip/$exe/ >> ip.$exe; echo >> ip.$exe
 	else
 		echo -e $R "$i is DOWN" $N
 	fi
 done
 
 read -p "Input URL or IP address: " target; ip=`ping -c1 $target | head -1 | awk '{print $3}' | tr -d '()'`
-curl https://ipapi.co/$ip/csv/ >> ip.csv; echo >> ip.csv
+curl https://ipapi.co/$ip/$exe/ >> ip.$exe; echo >> ip.$exe
 
-echo -e $Y"DONE!" $N
+echo -e $G"DONE!" $Y"Inspect your ip.$exe file"$N
