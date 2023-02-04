b33141364c23b021baea1b3555842b2a6e37f1c6
自由 <46299378+li-berty@users.noreply.github.com>
2023-02-04 15:47:55 +0300

    alias.sh

--- alias.sh
+++ alias.sh
@@ -13,9 +13,13 @@ alias q!="exit"
 alias ?="pwd"
 alias hi="history"
 alias ping="ping -c4"
+alias wifi="nmcli device wifi show-password"
 alias ipconfig="ip address | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
 alias wfp="sudo egrep -h -s -A 9 --color -T 'ssid=' /etc/NetworkManager/system-connections/*"
 alias speed="curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -"
+
+# JOKES
+# alias cd="echo 'Stay at home, you retard! Permission denied:'"
 
 EOF
 
